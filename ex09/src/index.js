c379391cdf8b1375c81775e7d34bf60b66605e0c
Er0rx <xer0rx123@gmail.com>
2021-03-12 22:13:29 +0100

    Meow Meow

---
+++ ex09/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+    var lastName = "Doe";
+    var fristLetterOfLastName = " ";
+    fristLetterOfLastName = lastName[0];
+    // Only change code above this line
+
+    return fristLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
